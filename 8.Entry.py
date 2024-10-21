cdd5a5fb1edd2d03633b569c2d7ff6f50c13fc17
Attica01 <atticadevelopers@gmail.com>
2024-10-21 16:42:20 +0530

    ttk entry field

--- 8.Entry.py
+++ 8.Entry.py
@@ -1,11 +1,13 @@
 from tkinter import *
-
+from tkinter import ttk
 root = Tk()
 
 root.geometry('500x500')
 
 entry = Entry(root, width=50)
+entry1 = ttk.Entry(root, width=50)
 
 entry.pack()
+entry1.pack()
 
 root.mainloop()
