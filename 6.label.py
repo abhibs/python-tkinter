ab3ef15582c877917c93e59ab3923145578e4c06
Attica01 <atticadevelopers@gmail.com>
2024-10-21 13:04:30 +0530

    updating label using config method

--- 6.label.py
+++ 6.label.py
@@ -5,7 +5,11 @@ root.geometry("500x500")
 
 label = Label(root, text="Abhiram B S")
 # updating label
-label['text'] = "Abhiram B S Javalli Tudoor Thirthahalli Shimoga karnataka"
+# label['text'] = "Abhiram B S Javalli Tudoor Thirthahalli Shimoga karnataka"
+
+# updating label using config method
+
+label.config(text="Abhiram B S Javalli Tudoor Thirthahalli Shimoga Karnataka")
 
 label.pack()
 
