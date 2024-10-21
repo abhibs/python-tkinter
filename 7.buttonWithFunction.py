287fd97d318fc12fcea4f45909f719a31fe36005
Attica01 <atticadevelopers@gmail.com>
2024-10-21 15:26:41 +0530

    label and button with funtion program

--- 7.buttonWithFunction.py
+++ 7.buttonWithFunction.py
@@ -3,8 +3,12 @@ from tkinter import *
 root = Tk()
 root.geometry('500x500')
 
+
+def changeName():
+    label.config(text="Abhiram B S Javalli Tudoor Thirthahalli Shimoga", fg='red', bg='blue')
+
 label = Label(root, text="Abhiram")
-button = Button(root, text="Update")
+button = Button(root, text="Update", command=changeName)
 
 label.pack()
 button.pack()
