8f9ac793176a84746b049de8a65b4073df43ae0a
Attica01 <atticadevelopers@gmail.com>
2024-10-21 12:55:12 +0530

    updating the label

--- 6.label.py
+++ 6.label.py
@@ -4,6 +4,9 @@ root = Tk()
 root.geometry("500x500")
 
 label = Label(root, text="Abhiram B S")
+# updating label
+label['text'] = "Abhiram B S Javalli Tudoor Thirthahalli Shimoga karnataka"
+
 label.pack()
 
 root.mainloop()
