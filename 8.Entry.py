bbb204263c95c15b7e7fec3fff43c7899bac9557
Attica01 <atticadevelopers@gmail.com>
2024-10-21 17:05:18 +0530

    placeholder in entry using insert method

--- 8.Entry.py
+++ 8.Entry.py
@@ -5,6 +5,11 @@ root = Tk()
 root.geometry('500x500')
 
 entry = Entry(root, width=50)
+
+# placeholder
+
+entry.insert(0, "Please Enter Your Name")
+
 entry1 = ttk.Entry(root, width=50)
 
 entry.pack()
