ae4d2851e8111277239abc9b7cdb904a3397c4ac
Attica01 <atticadevelopers@gmail.com>
2024-10-26 12:16:02 +0530

    grid program with grid function

--- 9.girdLayout.py
+++ 9.girdLayout.py
@@ -8,7 +8,7 @@ root.geometry("500x450")
 
 lbltitle = ttk.Label(text='Abhiram B S', font=(('Arial'), 22))
 lblname = ttk.Label(text='Your Name: ')
-lblpass = ttk.Label(text='Your Password')
+lblpass = ttk.Label(text='Your Password:')
 
 entry1 = ttk.Entry(root, width=30)
 entry2 = ttk.Entry(root, width=30)
@@ -18,5 +18,13 @@ entry2.insert(0, 'Enter Your Password')
 
 button = ttk.Button(root, text="Login")
 
+
+
+lbltitle.grid(row=0, column=0)
+lblname.grid(row=1, column=0)
+entry1.grid(row=1, column=1)
+lblpass.grid(row=2, column=0)
+entry2.grid(row=2, column=1)
+button.grid(row=3, column=0)
 
 root.mainloop()
