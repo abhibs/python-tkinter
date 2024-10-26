3a48786ce58a1113e0d250e2eb0b93234f4fca18
Attica01 <atticadevelopers@gmail.com>
2024-10-26 12:48:10 +0530

    lblname lable sticky west side given

--- 9.girdLayout.py
+++ 9.girdLayout.py
@@ -21,7 +21,7 @@ button = ttk.Button(root, text="Login")
 
 
 lbltitle.grid(row=0, column=0, columnspan=2)
-lblname.grid(row=1, column=0)
+lblname.grid(row=1, column=0, sticky=W)
 entry1.grid(row=1, column=1)
 lblpass.grid(row=2, column=0)
 entry2.grid(row=2, column=1)
