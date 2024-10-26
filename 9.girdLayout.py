6500c2192f6449692391f9f2ff23d432f995d767
Attica01 <atticadevelopers@gmail.com>
2024-10-26 12:11:03 +0530

    grid program without grid function

--- 9.girdLayout.py
+++ 9.girdLayout.py
@@ -4,4 +4,19 @@ from tkinter import ttk
 root = Tk()
 
 root.geometry("500x450")
+
+
+lbltitle = ttk.Label(text='Abhiram B S', font=(('Arial'), 22))
+lblname = ttk.Label(text='Your Name: ')
+lblpass = ttk.Label(text='Your Password')
+
+entry1 = ttk.Entry(root, width=30)
+entry2 = ttk.Entry(root, width=30)
+
+entry1.insert(0, 'Enter Your Name')
+entry2.insert(0, 'Enter Your Password')
+
+button = ttk.Button(root, text="Login")
+
+
 root.mainloop()
