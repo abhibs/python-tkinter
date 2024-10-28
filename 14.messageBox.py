bf5b454fb36abd3e72bada6397e2c57f3c10f8ab
abhibs <abhirambs97@gmail.com>
2024-10-28 17:54:36 +0530

    two buttons created

--- 14.messageBox.py
+++ 14.messageBox.py
@@ -5,5 +5,13 @@ from tkinter import messagebox
 root = Tk()
 root.geometry("350x250")
 
+button1 = ttk.Button(root, text='Button One')
+button2 = ttk.Button(root, text='Button Two')
+
+
+
+button1.grid(row=0, column=0, pady=25, padx=50)
+button2.grid(row=0, column=1)
+
 
 root.mainloop()
