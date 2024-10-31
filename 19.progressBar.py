061daf6ebd4745ee99b2eef5e3e2116feb289066
abhibs <abhirambs97@gmail.com>
2024-10-31 17:52:56 +0530

    progress bar with scale completed

--- 19.progressBar.py
+++ 19.progressBar.py
@@ -10,8 +10,15 @@ root.geometry("450x450+650+350")
 progress = ttk.Progressbar(root, orient=HORIZONTAL, length=200)
 progress.pack(pady=20)
 # progress.config(mode='indeterminate')
-progress.config(mode='determinate')
+progress.config(mode='determinate', maximum=50.0, value=10.0)
 progress.start()
+progress.stop()
 
+
+value = DoubleVar()
+progress.config(variable=value)
+
+scale = ttk.Scale(root, orient=HORIZONTAL,var=value, length=200, from_=0.0, to=50.0)
+scale.pack()
 
 root.mainloop()
