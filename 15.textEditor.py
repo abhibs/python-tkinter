f9714d3c0990b73a5cac691a8729baffcad09c8d
abhibs <abhirambs97@gmail.com>
2024-10-31 11:33:17 +0530

    state normal for text editor using config funtion

--- 15.textEditor.py
+++ 15.textEditor.py
@@ -8,6 +8,7 @@ texteditor = Text(root, width=30, height=10)
 texteditor.grid(row=0, column=0, padx=40, pady=20)
 texteditor.insert(INSERT, "Abhiram B S Javalli")
 texteditor.config(state='disabled')
+texteditor.config(state='normal')
 button = Button(root, text="Save", width=10, height=1)
 button.grid(row=3, column=0)
 
