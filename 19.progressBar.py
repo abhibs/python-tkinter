88be1fc230db18ea92c01af6275fd4c99d1c9a9d
abhibs <abhirambs97@gmail.com>
2024-10-31 17:25:34 +0530

    progress bar started

--- 19.progressBar.py
+++ 19.progressBar.py
@@ -7,7 +7,10 @@ root.title("Abhiram B S")
 root.geometry("450x450+650+350")
 
 
+progress = ttk.Progressbar(root, orient=HORIZONTAL, length=200)
+progress.pack(pady=20)
 
+progress.start()
 
 
 root.mainloop()
