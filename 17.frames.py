f0a34ad2a40fe8ddd986ea03615bdf9055f97f6b
abhibs <abhirambs97@gmail.com>
2024-10-31 16:16:42 +0530

    frames not showing

--- 17.frames.py
+++ 17.frames.py
@@ -5,4 +5,10 @@ root.geometry("650x650+450+200")
 
 frames = Frame(root, height=300, width=300, bg='blue')
 frames.pack()
+
+
+button1 = Button(frames, text='Button 1')
+button1.pack()
+
+
 root.mainloop()
