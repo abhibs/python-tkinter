02e28c25709672441bc918808b3c20e4eb3608d4
abhibs <abhirambs97@gmail.com>
2024-10-31 16:10:23 +0530

    once given bg color then only show the frames

--- 17.frames.py
+++ 17.frames.py
@@ -3,6 +3,6 @@ root = Tk()
 root.title("Abhiram B S")
 root.geometry("650x650+450+200")
 
-frames = Frame(root, height=300, width=300)
-
+frames = Frame(root, height=300, width=300, bg='blue')
+frames.pack()
 root.mainloop()
