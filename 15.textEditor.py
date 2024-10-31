05a4cf6ce8efa76547f283aaf515fd7ac3be5292
abhibs <abhirambs97@gmail.com>
2024-10-31 11:08:47 +0530

    padx and pady given to text editor

--- 15.textEditor.py
+++ 15.textEditor.py
@@ -5,6 +5,6 @@ root = Tk()
 root.geometry("550x550")
 
 texteditor = Text(root, width=30, height=10)
-texteditor.grid(row=0, column=0)
+texteditor.grid(row=0, column=0, padx=40, pady=20)
 
 root.mainloop()
