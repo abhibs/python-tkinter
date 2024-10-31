c7f984c4977d9f684bd35cb16993e39bc9b494a7
abhibs <abhirambs97@gmail.com>
2024-10-31 11:24:42 +0530

    insert data in text editor

--- 15.textEditor.py
+++ 15.textEditor.py
@@ -6,7 +6,7 @@ root.geometry("550x550")
 
 texteditor = Text(root, width=30, height=10)
 texteditor.grid(row=0, column=0, padx=40, pady=20)
-
+texteditor.insert(INSERT, "Abhiram B S Javalli")
 button = Button(root, text="Save", width=10, height=1)
 button.grid(row=3, column=0)
 
