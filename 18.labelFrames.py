bd9bce8de59b621c754cb14577a4984cea4a211a
abhibs <abhirambs97@gmail.com>
2024-10-31 17:01:26 +0530

    lable frames completed

--- 18.labelFrames.py
+++ 18.labelFrames.py
@@ -5,4 +5,14 @@ root.title("Abhiram B S")
 
 root.geometry("650x650+450+200")
 
+
+searchbar = LabelFrame(root, text="Search Box")
+searchLbl = Label(searchbar, text="Search")
+searchbar.pack(side=TOP, pady=30)
+
+entry = Entry(searchbar)
+entry.pack(side=LEFT, padx=10, pady=30)
+
+button = Button(searchbar, text="Search")
+button.pack(side=LEFT, padx=10)
 root.mainloop()
