0ca30cc0c0fd8e6175be116993750c2cd005cae5
abhibs <abhirambs97@gmail.com>
2024-11-11 16:42:39 +0530

    insert first using 0 to frame third

--- 20.PanedWindow.py
+++ 20.PanedWindow.py
@@ -10,9 +10,12 @@ root.geometry("650x650+650+200")
 pw = ttk.PanedWindow(root, orient=HORIZONTAL)
 frame1 = ttk.Frame(pw, width=100, height=500, relief=SUNKEN)
 frame2 = ttk.Frame(pw, width=300, height=500, relief=SUNKEN)
+frame3 = ttk.Frame(pw, width=75, height=500, relief=SUNKEN)
 
 pw.add(frame1)
 pw.add(frame2)
+
+pw.insert(0, frame3)
 
 pw.pack(fill=BOTH)
 
