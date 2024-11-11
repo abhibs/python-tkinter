af9167a2f1677d199be31ed60b384b9e2a74a5b2
abhibs <abhirambs97@gmail.com>
2024-11-11 16:06:51 +0530

    two panel window created

--- 20.PanedWindow.py
+++ 20.PanedWindow.py
@@ -7,5 +7,13 @@ root = Tk()
 
 root.geometry("650x650+650+200")
 
+pw = ttk.PanedWindow(root, orient=HORIZONTAL)
+frame1 = ttk.Frame(pw, width=100, height=500, relief=SUNKEN)
+frame2 = ttk.Frame(pw, width=300, height=500, relief=SUNKEN)
+
+pw.add(frame1)
+pw.add(frame2)
+
+pw.pack()
 
 root.mainloop()
