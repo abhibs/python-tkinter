3aa6a47b5fbead3062fd454792db7f433f991ee6
abhibs <abhirambs97@gmail.com>
2024-11-12 17:21:00 +0530

    tabs are created

--- 21.tabs.py
+++ 21.tabs.py
@@ -4,4 +4,16 @@ from tkinter import ttk
 root = Tk()
 
 root.geometry("650x650+650+200")
+
+
+tabs = ttk.Notebook(root)
+
+tab1 = ttk.Frame(tabs)
+tab2 = ttk.Frame(tabs)
+
+tabs.add(tab1, text="First Tab")
+tabs.add(tab2, text="Second Tab")
+
+tabs.pack()
+
 root.mainloop()
