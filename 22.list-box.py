e7c84e385f3814df0efa139fc38d7919f985c4ca
abhibs <abhirambs97@gmail.com>
2024-11-14 12:06:13 +0530

    adding list item in the listbox

--- 22.list-box.py
+++ 22.list-box.py
@@ -5,6 +5,19 @@ root = Tk()
 root.geometry("650x650+650+200")
 
 listbox = Listbox(root)
+listbox.insert(0, "Abhiram")
+listbox.insert(1, "Anjan")
+listbox.insert(2, "Aravind")
+listbox.insert(3, "Anitha")
+listbox.insert(4, "Anushree")
+listbox.insert(5, "Venkatesh Prasad")
+listbox.insert(6, "Krishna Prasad")
+listbox.insert(7, "Saritha")
+listbox.insert(8, "Surabhi")
+listbox.insert(9, "Manu")
+listbox.insert(10, "Anuradha")
+
+
 listbox.pack()
 
 root.mainloop()
