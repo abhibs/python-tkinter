dab344cc98fbc649c1998b889b45cf9e7abdefbb
abhibs <abhirambs97@gmail.com>
2024-11-14 14:32:31 +0530

    File Menu created

--- 24.menubar.py
+++ 24.menubar.py
@@ -4,4 +4,11 @@ root = Tk()
 root.geometry("650x650+650+250")
 root.title("Abhiram B S")
 
+menuBar = Menu(root)
+root.config(menu=menuBar)
+
+file = Menu(menuBar)
+menuBar.add_cascade(label='File', menu=file)
+
+
 root.mainloop()
