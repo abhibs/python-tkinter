59edf80618d0ffa5e8273fa026976256dbc089ee
abhibs <abhirambs97@gmail.com>
2024-11-14 15:17:57 +0530

    exit function created with messagebox

--- 24.menubar.py
+++ 24.menubar.py
@@ -1,8 +1,14 @@
 from tkinter import *
-
+from tkinter import messagebox
 root = Tk()
 root.geometry("650x650+650+250")
 root.title("Abhiram B S")
+
+
+def exitFunction():
+    mbox = messagebox.askquestion("Exit", "Are You Sure To Exit ?", icon='warning')
+    if mbox == 'yes':
+        root.destroy()
 
 menuBar = Menu(root)
 root.config(menu=menuBar)
@@ -17,6 +23,6 @@ file.add_separator()
 file.add_command(label="Save")
 file.add_separator()
 icon = PhotoImage(file='icons/exit.png')
-file.add_command(label="Exit", image=icon, compound=LEFT)
+file.add_command(label="Exit", image=icon, compound=LEFT, command=exitFunction)
 
 root.mainloop()
