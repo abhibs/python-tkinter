dc106fe87fb036882fd504a417a466454873642d
abhibs <abhirambs97@gmail.com>
2024-11-14 13:18:49 +0530

    image show in the window

--- 23.image.py
+++ 23.image.py
@@ -6,4 +6,13 @@ root.title("Abhiram B S")
 
 root.geometry("350x350")
 
+lbl = Label(root,text="Abhiram B S")
+logo = PhotoImage(file='icons/Abhi.png')
+lblImage = Label(root, image=logo)
+
+
+lbl.pack()
+lblImage.pack()
+
+
 root.mainloop()
