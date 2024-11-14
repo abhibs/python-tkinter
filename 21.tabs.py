5c6c47f9b9d03fe4e809950857923dd3b6356e8b
abhibs <abhirambs97@gmail.com>
2024-11-14 11:26:10 +0530

    icons add

--- 21.tabs.py
+++ 21.tabs.py
@@ -6,13 +6,15 @@ root = Tk()
 root.geometry("650x650+650+200")
 
 
+icon = PhotoImage(file='icons/favicon.png')
+
 tabs = ttk.Notebook(root)
 
 tab1 = ttk.Frame(tabs)
 tab2 = ttk.Frame(tabs)
 
-tabs.add(tab1, text="First Tab")
-tabs.add(tab2, text="Second Tab")
+tabs.add(tab1, text="First Tab", image=icon, compound=LEFT)
+tabs.add(tab2, text="Second Tab", image=icon, compound=LEFT)
 
 tabs.pack(fill=BOTH)
 
