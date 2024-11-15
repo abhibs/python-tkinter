90f79be334897e8597ace2d65c8cfcabb63edcf7
abhibs <abhirambs97@gmail.com>
2024-11-15 10:35:21 +0530

    insert item into tree view

--- 25.treeView.py
+++ 25.treeView.py
@@ -10,5 +10,10 @@ root.geometry("650x650+650+250")
 treeview = ttk.Treeview(root)
 treeview.pack()
 
+treeview.insert('', '0', 'item1', text='First Item')
+treeview.insert('', '1', 'item2', text='Second Item')
+treeview.insert('', '2', 'item3', text='Third Item')
+treeview.insert('', '3', 'item4', text='Forth Item')
+treeview.insert('', '4', 'item5', text='Fifth Item')
 
 root.mainloop()
