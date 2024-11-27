9bc19aeb7cb14f43fe76bd56dd46886a58074939
abhibs <abhirambs97@gmail.com>
2024-11-27 12:37:23 +0530

    scroll bar completed

--- 26.scroll-bar.py
+++ 26.scroll-bar.py
@@ -10,4 +10,8 @@ root.geometry("500x450")
 textBox = Text(root, width=60, height=20, wrap=WORD)
 textBox.grid(row=0, column=0)
 
+scroll = ttk.Scrollbar(root, orient=VERTICAL, command=textBox.yview)
+scroll.grid(row=0, column=1, sticky=N+S)
+textBox.config(yscrollcommand=scroll.set)
+
 root.mainloop()
